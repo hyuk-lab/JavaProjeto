4350820d335bb4cf10f3a6f119e846cf4179ef20
hyuk-lab <goliveiramisael18@gmail.com>
2024-08-19 16:36:27 -0300

    legal

---
+++ Teste/src/ForCounter.java
@@ -0,0 +1,11 @@
+public class ForCounter {
+    public static void main(String[] args){
+        for (int i = 77;i >=7; i-= 7)
+            System.out.printf("%d ",i);
+
+        System.out.println();
+    }
+
+
+
+}
